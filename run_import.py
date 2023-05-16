5734b41c601773c1fe53139a3fa9bb6ba5d5d2bc
tanne <tvh8@nau.edu>
2023-05-15 20:02:09 -0700

    trying to add everything to log

--- run_import.py
+++ run_import.py
@@ -1,14 +1,14 @@
 import sys
 print(sys.path)
 
-from DCTA.import_csv import import_csv_to_db, import_analysis_csv_to_db, import_events_csv_to_db
-from DCTA.app import create_app
-from DCTA.database import db
+from import_csv import import_csv_to_db, import_analysis_csv_to_db, import_events_csv_to_db
+from app import create_app
+from database import db
 
 app = create_app()
 
 with app.app_context():
-    import_csv_to_db(db, 'dcta/output.csv')
-    import_csv_to_db(db, 'dcta/analysis_output.csv')
-    import_csv_to_db(db, 'dcta/events.csv')
+    import_csv_to_db(db, 'output.csv')
+    import_csv_to_db(db, 'analysis_output.csv')
+    import_csv_to_db(db, 'events.csv')
     df.columns = df.columns.str.strip()
