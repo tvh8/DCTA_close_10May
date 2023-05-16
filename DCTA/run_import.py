568e29894d7ba4d2c9bbb55c70af3f44195ac362
tanne <tvh8@nau.edu>
2023-05-16 09:51:50 -0700

    trying to add everything to log

--- DCTA/run_import.py
+++ DCTA/run_import.py
@@ -5,10 +5,12 @@ from import_csv import import_csv_to_db, import_analysis_csv_to_db, import_event
 from app import create_app
 from database import db
 import pandas as pd
+from models import Bill, Analysis, Event
 
 app = create_app()
 
 with app.app_context():
+    db.create_all()
     import_csv_to_db('output.csv')
     import_analysis_csv_to_db('analysis_output.csv')
     import_events_csv_to_db('events.csv')
