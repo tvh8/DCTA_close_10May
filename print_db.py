e68cc9d7420860c4efb5e5d88fe07a6e63a8f3de
tanne <tvh8@nau.edu>
2023-05-17 08:27:20 -0700

    added HTML

--- print_db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, inspect
-
-engine = create_engine('sqlite:///site.db')
-inspector = inspect(engine)
-
-# Get table information
-print(inspector.get_table_names())
